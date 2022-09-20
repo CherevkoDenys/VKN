e29ee83e7b95eba567cffe723ce7a5010435fd7c
CherevkoDenys <113729276+CherevkoDenys@users.noreply.github.com>
2022-09-20 14:11:02 +0300

    Delete lab4.2(1).py

--- lab4.2(1).py
+++
@@ -1,8 +0,0 @@
-import math
-def func1(x1,x2):
-    U=((x1)*math.log10((x2)))+(1/((x1)**2+(x2)**2+0.3))-math.e**(6*(x1)-(x2))
-    return(U)
-x, y=float(input("введіть текст :")), float(input("введіть текст :"))
-k=func1(x,y)
-print(U)
-func1(x,y)
