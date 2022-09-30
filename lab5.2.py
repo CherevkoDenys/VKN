4e599725120dfc6347fbe10f9713fbcb33a7948c
CherevkoDenys <113729276+CherevkoDenys@users.noreply.github.com>
2022-09-30 12:23:26 +0300

    Add files via upload

---
+++ lab5.2.py
@@ -0,0 +1,11 @@
+import math
+A=float(input("введіть перше число:    "))
+B=float(input("введіть друге число:    "))
+C=float(input("введіть третє число:    "))
+O=0.0
+if B>A and C>A:
+   print('А-найменше')
+if B>C and A>C:
+   print('С-найменше')
+if C>B and A>B: 
+   print('В-найменше')
